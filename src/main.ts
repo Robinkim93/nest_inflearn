ca5a8e6df66483a992e99a749ab6de4ffa6674ea
Robinkim93 <Robinkim93@gmail.com>
2022-11-29 20:10:37 +0900

    env삭제

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
